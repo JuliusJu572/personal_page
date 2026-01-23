1ee17715682a231b5e8fb5dc3539014eb9d7c123
JuliusJu <jrb572572@163.com>
2026-01-24 01:18:43 +0800

    init

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'Mystic_AI_Tarot']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
